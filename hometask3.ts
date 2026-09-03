1e54ebdd959bbb25758d4cb68456221803d060be
Andrew199117 <andrey91stalker@gmail.com>
2026-09-04 00:48:16 +0300

    feat: pull request fixes

--- hometask3.ts
+++ hometask3.ts
@@ -2,12 +2,7 @@ const startCount: number = 100;
 const endCount: number = 0;
 let step: number = 1;
 
-for (let counter = startCount; counter >= endCount; ) {
+for (let counter = startCount; counter >= endCount; counter -= step, step++) {
   console.log(counter);
-  counter = counter - step;
-  step++;
-  if (endCount < 0) {
-    console.log(0);
-    break;
-  }
 }
+console.log(endCount);
